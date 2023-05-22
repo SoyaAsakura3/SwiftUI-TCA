43fc375c1adbee3a9af7f084de6347f3a5e999fb
SoyaAsakura <s.asakura@arsaga.jp>
2023-05-22 11:50:59 +0900

    Xcodeプロジェクト作成

---
+++ SwiftUI-TCA/SwiftUI-TCA/SwiftUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCAApp.swift
+//  SwiftUI-TCA
+//
+//  Created by s.asakura on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
