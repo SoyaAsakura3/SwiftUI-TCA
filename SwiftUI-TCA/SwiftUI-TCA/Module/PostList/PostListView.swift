5bf23f7ef4acbbcac4504c273e99484cbca22efc
SoyaAsakura <s.asakura@arsaga.jp>
2023-05-25 14:59:46 +0900

    qiitaの記事を表示するViewを追加

---
+++ SwiftUI-TCA/SwiftUI-TCA/Module/PostList/PostListView.swift
@@ -0,0 +1,20 @@
+//
+//  PostListView.swift
+//  SwiftUI-TCA
+//
+//  Created by s.asakura on 2023/05/25.
+//
+
+import SwiftUI
+
+struct PostListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostListView()
+    }
+}
